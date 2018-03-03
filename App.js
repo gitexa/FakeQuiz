8184909dab43da78738f33265e9b555189f2813c
gitexa <uafla@student.kit.edu>
2018-03-03 22:47:13 +0100

    Inital working project

--- App.js
+++ App.js
@@ -15,17 +15,6 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Finally!</Text>
 
-        <Swiper style={styles.wrapper} showsButtons={true}>
-  <View style={styles.slide1}>
-    <Text style={styles.text}>Hello Swiper</Text>
-  </View>
-  <View style={styles.slide2}>
-    <Text style={styles.text}>Beautiful</Text>
-  </View>
-  <View style={styles.slide3}>
-    <Text style={styles.text}>And simple</Text>
-  </View>
-</Swiper>
       </View>
 
     );
